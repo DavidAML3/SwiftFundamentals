771b78a5720ec23be4ae9322253db9a9ba775f9a
David Andres Mejia Lopez <davidaml@Davids-MacBook-Pro.local>
2024-09-26 17:34:26 -0500

    finished setting up xcode and added an introduction to the proyect and history of swift

---
+++ Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,55 @@
+//: [Previous](@previous)
+
+// MARK: - Historia de Swift
+///
+/// Swift es un lenguaje de programacion creado por apple, lanzado por primera vez en junio de 2014
+/// fue liderado por chris lattner, quien queria ofrecer una alternativa mas moderna y segura a Objective-C.
+///
+/// Antes de Swift, Objective-C era el estandar para el desarrollo en el ecosistema de Apple,
+/// pero este presentaba ciertas limitaciones, como la complejidad de su sintaxis y la propension a errores
+/// Apple queria crear un lenguaje que fuera mas facil de aprender, segro y eficiente, asi Swift fue disenado para
+/// ser intuitivo, moderno y con un enfoque en la programacion orientada a objetos y funcional.
+///
+// MARK: - Caracteristicas
+///
+/// - Sintaxis sencilla: Swift tiene una sintaxis mas limpia y legible, lo que facilita el
+/// aprendizaje para nuevos programadores.
+///
+/// - Seguridad: Introduce caracteristicas como la opcion de tipos y la gestion de memoria automatica,
+/// lo que ayuda a prevenir errores comunes en la programacion.
+///
+/// - Rendimiento: Swift esta optimizado para ser rapido y eficiente, comparable a c++ en termino
+/// de rendimiento en muchos casos.
+///
+/// - Interoperabilidad: Swift se diseno para coexistir con Objective-C, permitiendo a los
+/// desarrolladores integrar ambos lenguajes en sus proyectos.
+///
+// MARK: - Evolucion
+///
+/// Desde su lanzamiento, Swift ha evolucionado rapidamente. en 2015, Apple hizo que Swift fuera de codigo
+/// abierto, lo que permitio a la comunidad contribuir a su desarrollo y expansion.
+///
+// MARK: - Swift history
+///
+/// Swift is a programming language created by Apple, first launched in june 2014. its development was led by Chris Lattner,
+/// who aimed to provide a modern and safer alternative to Objective-C.
+///
+/// Before Swift, Objective-C was the standard for Apple's ecosystem, but it had certain limitations, such as complex
+/// syntax and a tendency for errors. Apple wanted to create a language that was easier to learn, safe, and efficient.
+/// Thus, Swift was designed to be intuitive, modern, and focysed on both object-oriented and functional programming.
+///
+// MARK: - Key features
+///
+/// - simple syntax: Swift has a cleaner and more readable syntax, making it easier for new programmers to learn.
+///
+/// - Safety: It introduces features like optional types and automatic memory management, helping to prevent common programming errors.
+///
+/// - Performance: Swift is optimized for speed and efficiency, comparable to C++ in performance for many cases.
+///
+/// - Interoperability: Swift was designed to coexist with Objective-C, allowing developers to integrate both languages in their projects.
+///
+// MARK: Evolution
+///
+/// Since its launch, Swift has evolved rapidly. in 2015, Apple made Swift open source, allowing the community to contribute to its development and expansion.
+
+//: [Next](@next)
